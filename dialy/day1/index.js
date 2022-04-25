fae012f1952ae29ed31202f26c4fd98fd5627166
FASNA-KA <jasmeeazeez813@gmail.com>
2022-04-25 17:54:46 +0530

    Java Script

---
+++ dialy/day1/index.js
@@ -0,0 +1,12 @@
+console.log("working...")
+
+console.log("Hello World!!")
+
+var number=2;
+function Square(n) {
+    var result = n*n ;
+    return result;
+}
+
+var newnumber = Square(3);
+console.log(newnumber);
---
+++ dialy/day2/index.js
@@ -0,0 +1,97 @@
+console.log("working...")
+
+let fName = "Sidhant";//declared and initialized in the same line
+let fname;
+fname = "Lakhi";
+console.log(fname);
+fname = "Lakhwinder";
+console.log(fname);
+
+const lname = "Gupta";
+// lname = "Vinci";
+console.log(lname);
+
+var age = 25;
+// console.log(age);
+
+var age;
+age = 26;
+// console.log(age);
+age = 27;
+console.log(age);
+
+var age = 29;
+console.log(age);
+
+
+
+// Boolean - (True /False), Strings - (Words, sentence, mix of numbers and words(ex - address)), Number - (integer and decimal values), 2 ,4, 101, 23.50, 34.99 
+
+"2, somelane, somecity - 711101"
+'2, somelane, somecity - 711101'
+"SIdhant", "907654321"
+
+// data types present in JS(Primitive)
+// WHat are Boolean Values - It has only two values True/False
+
+// What are Strings - Anything which is present inside the double quotes or the single  quotes are called as strings
+
+// Numbers - includes Integer and decimal values (2/3/34.66/99.99)
+
+// Variables - They are used to store the values which could be of any type i.e. Number type or String Type or Boolean type.
+
+// rules for assigning names to variables
+// try to make the variables name declarative
+// it uses Camel Case
+// Variable names are case sensitive 
+
+// fName, fname, Fname, fnAme, fnaMe, 
+
+// Keywords are of three types
+// let, var, const
+
+// -----declaraing / initializing/ redeclaring----
+
+// let - These keywords are used to as
+// let - These keywords are used to assign variables with values
+// 1 . They can be declared in a line and then initialized with a value in anaother lines.
+// 2. They can be also declared and initialized in the same line
+// 3 . They cannot be redeclared.....But can be reinitialized....
+// 4. ******We cannot have Two Variables with same name in Let*******
+
+let fName = "Sidhant";//declared and initialized in the same line
+let fname;
+fname = "Lakhi";
+console.log(fname);
+fname = "Lakhwinder";
+console.log(fname);
+
+// const - These keywords are used to assign variables with values
+// 1. They can be declared and initialized in the same line only.
+// 2 . They cannot be redeclared.....
+// 3. But const "cant" be reinitialized also....
+// 4. ******We cannot have Two Variables with same name in const*******
+
+const lname = "Gupta";
+// lname = "Vinci";
+console.log(lname);
+
+// var - These keywords are used to assign variables with values
+// var - These keywords are used to assign variables with values
+// 1 . They can be declared in a line and then initialized with a value in another lines.
+// 2. They can be also declared and initialized in the same line
+// 3. They can be redeclared.....and also can be reinitialized....
+// 4. ******We can have Two or more Variables with same name in Var*******
+
+var age = 25;
+// console.log(age);
+
+var age;
+age = 26;
+// console.log(age);
+age = 27;
+console.log(age);
+
+var age = 29;
+console.log(age);
+
