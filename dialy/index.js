6756765f5f1c82769ba4de2f0f3ab214ab61cb06
Fasna-KA <102172133+Fasna-KA@users.noreply.github.com>
2022-04-28 20:40:24 +0530

    Add files via upload

--- dialy/index.js
+++ dialy/index.js
@@ -1,81 +1,111 @@
-//loop- for,while,do while,for each,for-in
 
-//for--initialise,condition checking,increment
-for(let i=0;i<10;i++){
-    console.log(i);
+// // Functions - In order to execute a particular lines of code repetitively, we can allocate them into a function, which can then be called again and again as required. This prevents us writing a piece of code mulitple times. 
+
+// //function definition from lines 6-8
+// //we need to have n no of parameters for n number of arguments passed while calling the functions, so parameters catch the arguments in the sequence they are passed
+// function multiply(parameter1, parameter2){
+//     console.log("Inside multiply function");
+//     //the function body can have more than one line of code.
+// }
+
+// //function calling on line 11
+// multiply(arg1,arg2);//name of a function followed by open and close parenthesis.
+// //while we call a function we pass arguments inside the parenthesis, if we have to pass any values otherwise not necessary to pass arguments. The arguments passed can be one or many separated by commas.
+
+// const addition = (parameter1 , parameter2) => {
+//     console.log("i am in addition function");
+//     }
+//     addition(argument1,argument2);
+    
+//     // function expression
+//     const subtraction = function(){
+//         console.log("i am in subtraction function")
+//     }
+//     subtraction(argument1 , argument2);
+
+function SayHi() {
+    console.log("Hi!!!");
+    let a=3;
+    let b=6;
+    let result=a+b;
+    console.log(result)
+    
+}  
+SayHi();  //calling the ftn
+
+function addition(num1,num2) {
+    let result=num1+num2;
+    console.log(result); 
 }
-//statement can be executed multiplt times 
-//we use let,const shows error
-//for(){console.log()}
-//starts with i=0 and end till i<10
-//need upto 20 ,<=20
+addition(3,4);
 
-
-let a=10;
-while(a>0){
-    console.log(a);
-    a--;//alwys write it in end of loop
+function multiply(n1,n2) {
+    let result=n1*n2;
+    return result; //dnt print ans,jst return the result and thrown to outside
+ 
 }
+//to see result
+let returnedresult=multiply(5,6);
+console.log(returnedresult);
 
-//using for loop try to print multiple of 2 (2,4,6...20)
+//other way
+console.log(multiply(5,6));
+    
 
-for(let j=1;j<=10;j++){
-    console.log(j*2);
+//The Lexical Environment and Scope Chain
+
+function one() {
+    // console.log(i);//wont print,coz i not defined,it wont take from child
+    two();
+    function two() {
+      let i=5;
+      console.log(i);  
+    }
 }
+one();
 
-//2 ways 2*a or a=a+2
-let d=2;
-while(d<21){
-    console.log(d);
-    d=d+2;
+//Block Scope and Shadowing
+
+let f=3;
+const g=4;
+var h=5;
+console.log(f);//3
+console.log(g);//4
+console.log(h);//5
+{
+    let f=4;
+const g=4;
+var h=4;
+
+    console.log(f);//4
+    console.log(g);//4
+    console.log(h);//4
+
 }
+console.log(f);//3
+    console.log(g);//4
+    console.log(h);//4--its becoz VAR ,it overwritten
 
-//do while
-//let=2,define c first
-//initialise>print>increment>condition checking
 
-let c=2
-do {
-    console.log(c);
-    c=c+2;
-} while (c<=20);
+// let m = 4;
+// console.log(m);//4
+// {
+//     let m= 5
+//     console.log(m);//5
 
-//for-in loop--give us index
+// }
+// console.log(m);//4
 
-//Array ["",""];
-//in -gives array
-//b-variable name
-let names=["Althaf","Azhar","Fasna","Jasmine","Azeez","Safna"];
+// const n = 4;
+// console.log(n);//4
+// {
+//     const n= 5
+//     console.log(n);//5
 
-for(let b in names){
-    console.log(b);
-}
+// }
+// console.log(n);//4
 
-//for-of loop-- give us values
-//of-gives values
-//values-variable name
 
-for(let values of names){
-    console.log(values);
-}
-
-//for- each loop --
-
-names.forEach((values,index)=>{//if we give value/intex first in both, we get value and then index
-    console.log(values,index)
-})
-//value then ,index n0
-
-names.forEach((values,index)=>{//if we give names alternate,we get index then 'value'
-    console.log(index,values)
-})
-
-names.forEach((index,values)=>{
-    console.log(values,index)
-})
-names.forEach((index,values)=>{
-    console.log(index,values)
-})
 
 
 
