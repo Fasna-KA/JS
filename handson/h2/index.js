048f4905e6769bb3ffce34e12e0821f753135d01
Fasna-KA <102172133+Fasna-KA@users.noreply.github.com>
2022-04-28 23:27:09 +0530

    Add files via upload

---
+++ handson/h2/index.js
@@ -0,0 +1,29 @@
+//6. p||P
+//7.if(A>B&&C){
+  return "2"
+}  
+else if(B>A&&C){
+  return "5"
+}
+else if(C>A&&B){
+  return "4"
+}
+else if(A==B==C){
+  return "-1"
+}
+else{ 
+  return "null"
+}
+
+//8.if(y<x<z){
+  return "x";
+}
+else if(x<y<z){
+  return "y";
+}
+else if(x<z<y){
+  return "z";
+}
+else{
+  return "null";
+}
