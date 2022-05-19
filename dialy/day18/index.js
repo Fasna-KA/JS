1ddd57b252aceda0eb8e3e1e29486adfc25f772c
Fasna-KA <102172133+Fasna-KA@users.noreply.github.com>
2022-05-19 21:38:49 +0530

    Add files via upload

---
+++ dialy/day18/index.js
@@ -0,0 +1,12 @@
+//Jquery - JS library which provides effective method for doing something in frontend and gives essentisl features like DOM manipulation ,AJAX interaction and so on...
+
+$("button").click(function(){
+    // $("p").hide();//will hide p part--and wont appear again ,aftr refrshng itll appear
+    $("p").toggle();//it will appear again if we click hide button again
+    // // $("#learn").toggle();
+    // $(".learn").toggle();
+
+})
+
+
+
